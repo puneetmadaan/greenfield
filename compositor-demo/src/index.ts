f1612088c4273e5e23ecb0e519b0aea48495fac9
Erik De Rijcke <derijcke.erik@gmail.com>
2020-12-31 15:40:21 +0100

    xwayland

--- compositor-demo/src/index.ts
+++ compositor-demo/src/index.ts
@@ -116,6 +116,8 @@ async function main() {
   remoteKwriteURLButton.textContent = 'KWrite'
   const remoteGnomeTerminalURLButton: HTMLButtonElement = document.createElement('button')
   remoteGnomeTerminalURLButton.textContent = 'Gnome-Terminal'
+  const remoteXtermURLButton: HTMLButtonElement = document.createElement('button')
+  remoteXtermURLButton.textContent = 'XTerm'
 
   const urlInput: HTMLInputElement = document.createElement('input')
   urlInput.type = 'text'
@@ -130,6 +132,7 @@ async function main() {
   container.appendChild(remoteGtk3URLButton)
   container.appendChild(remoteKwriteURLButton)
   container.appendChild(remoteGnomeTerminalURLButton)
+  container.appendChild(remoteXtermURLButton)
   container.appendChild(urlInput)
   container.appendChild(launchButton)
 
@@ -139,6 +142,7 @@ async function main() {
   remoteGtk3URLButton.onclick = () => urlInput.value = `ws://localhost:8081?launch=remote-gtk3-demo`
   remoteKwriteURLButton.onclick = () => urlInput.value = `ws://localhost:8081?launch=remote-kwrite`
   remoteGnomeTerminalURLButton.onclick = () => urlInput.value = `ws://localhost:8081?launch=remote-gnome-terminal`
+  remoteXtermURLButton.onclick = () => urlInput.value = `ws://localhost:8081?launch=remote-xterm`
 
   launchButton.onclick = () => {
     const urlString = urlInput.value
